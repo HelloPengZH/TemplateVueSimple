d9d724ec107164b38cc355a4f5568048238d3bae
Mr.RightMeet <971107503@qq.com>
2018-01-07 17:24:33 +0800

    Vue简单模板,已完善less预处理

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+        <span class='red' v-text='name'>
+          主页面App.Vue组件展示页面
+        </span>
+        <!-- app.vue页面主页面展示内容 -->
+        <div>
+          子组件展示页面展示页面
+        </div>
+        <div>
+          子组件路由
+        </div>
+        <router-link to="/login">登录</router-link>
+        <router-link to="/register">注册</router-link>
+        <div>子组件展示</div>
+        <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+      return {
+          name: '你好,try everything'
+      }
+    },
+  methods:{
+
+  },
+  created(){
+
+  }
+}
+</script>
+<style scoped>
+    .red {
+       background-color: #213212 
+    }
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,51 @@
+import Vue from 'vue';
+//引入vue
+import App from './App.vue';
+//引入 注主模块
+
+//注入分模块
+
+
+// 4.0 注册mint-ui
+// 导入mint-ui的css文件
+
+
+// 5.0 注册mui的css样式
+import '../statics/mui/css/mui.css';
+
+// 6.0 导入一个当前系统的全局基本样式
+import '../statics/css/site.css';
+
+// 7.0 将vue-resource在vue中绑定，自动在vue实例对象上注入一个$http对象就可以使用ajax方法请求数据
+
+
+import vueRouter  from 'vue-router';
+Vue.use(vueRouter)
+
+import login  from './components/account/login.vue';
+import register  from './components/account/register.vue';
+
+
+
+var router = new vueRouter({
+    linkActiveClass:'miu-active',
+    routes:[
+        {
+            path:"/login",
+            component:login
+        },
+        {
+            path:"/register",
+            component:register
+        }
+    ]
+})
+
+new Vue({
+	el:'#app',
+    render: function (create) {
+        return create(App)
+    }, //es5的写法
+    // render:c=>c(App)  // es6的函数写法 =>：goes to
+    router
+});
